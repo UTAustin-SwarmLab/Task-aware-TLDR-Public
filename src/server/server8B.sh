d8d638425efef5912a4dbb11c13d1e75dc340c7f
Po-han Li <34739111+d31003@users.noreply.github.com>
2025-05-23 10:28:08 +0800

    Update and rename server8B.sh

--- src/server/server8B.sh
+++ src/server/server8B.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MODEL="OpenGVLab/InternVL2_5-8B-MPO"
+MODEL="OpenGVLab/InternVL3-8B"
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export NCCL_P2P_DISABLE=1
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
